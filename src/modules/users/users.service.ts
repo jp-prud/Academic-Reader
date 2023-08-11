b638aaee97ca8a47e79b59687fcea66fc6b5842d
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-08-11 14:51:29 -0300

    feat: create initial crud to user entity

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,56 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { hash } from 'bcryptjs';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, name } = createUserDto;
+
+    const emailTaken = await this.usersRepository.findByEmail(email);
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepository.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        posts: {
+          create: {
+            title: 'Welcome 🙂!',
+            description: 'This is a simple description to your first post.',
+            content: `Welcome, ${name}!! This is your first post!`,
+            type: 'ARTICLE',
+          },
+        },
+      },
+    });
+
+    return {
+      id: user.id,
+    };
+  }
+
+  findAll() {}
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
