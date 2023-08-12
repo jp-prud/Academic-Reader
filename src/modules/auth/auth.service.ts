39d91e279aeaea2f61804f4ad66e6c0df85ceee8
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-08-12 10:47:34 -0300

    feat: create auth module

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,78 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare, hash } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signin(signInDto: SigninDto) {
+    const { email, password } = signInDto;
+
+    const user = await this.usersRepository.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = await this.generateAccessToken(user.id);
+
+    return { accessToken };
+  }
+
+  async signup(signupDto: SignupDto) {
+    const { email, password, name } = signupDto;
+
+    const emailTaken = await this.usersRepository.findByEmail(email);
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepository.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        posts: {
+          create: {
+            title: 'Welcome 🙂!',
+            description: 'This is a simple description to your first post.',
+            content: `Welcome, ${name}!! This is your first post!`,
+            type: 'ARTICLE',
+          },
+        },
+      },
+    });
+
+    const accessToken = await this.generateAccessToken(user.id);
+
+    return { accessToken };
+  }
+
+  private async generateAccessToken(userId: string) {
+    return this.jwtService.signAsync({ sub: userId });
+  }
+}
