551bcc04990563af2b31133507178bc5684da8ad
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-09-21 16:09:26 -0300

    feat: add comments repository as provider in database module

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,9 +2,15 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
 import { PostsRepository } from './repositories/posts.repositories';
+import { CommentsRepository } from './repositories/comments.repositories';
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, PostsRepository],
-  exports: [UsersRepository, PostsRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    PostsRepository,
+    CommentsRepository,
+  ],
+  exports: [UsersRepository, PostsRepository, CommentsRepository],
 })
 export class DatabaseModule {}
